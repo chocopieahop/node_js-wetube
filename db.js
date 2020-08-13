cb4ef12da1d548281fef724689b1a933f564469a
JeongEun <6rodnjf1@naver.com>
2020-08-13 23:58:36 +0900

    11

---
+++ db.js
@@ -0,0 +1,39 @@
+export const videos = [
+  {
+    id: 123123,
+    title: "Video awesome",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4",
+    creator: {
+      id: 321321,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 222222,
+    title: "Video nice",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4",
+    creator: {
+      id: 321321,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 111111,
+    title: "Video super",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4",
+    id: 321321,
+    name: "Nicolas",
+    email: "nico@las.com",
+  },
+];
