7aaff46c39085e35077b2ce02e936b7c921bb795
JeongEun <6rodnjf1@naver.com>
2020-08-20 22:40:40 +0900

    mini extract

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
-const ExtractCSS = require("extract-text-webpack-plugin");
+
+// const ExtractCSS = require("extract-text-webpack-plugin");
+const MiniExtractCSS = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
-const OUTPUT_DIR = path.join(__dirname, "static");
+const OUTPUT_DIR = path.resolve(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
@@ -12,21 +15,41 @@ const config = {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractCSS.extract([
+        use: [
           {
-            loader: "css-loader",
+            loader: MiniExtractCSS.loader,
+            options: {
+              hmr: process.env.WEBPACK_ENV === "development",
+            },
           },
+          "css-loader",
           {
             loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [
+                  autoprefixer({
+                    overrideBrowserslist: "cover 99.5%",
+                  }),
+                ];
+              },
+            },
           },
-          {
-            loader: "sass-loader",
-          },
-        ]),
+
+          "sass-loader",
+        ],
       },
     ],
   },
-  output: { path: OUTPUT_DIR, filename: "[name].js" },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [
+    new MiniExtractCSS({
+      filename: "styles.css",
+    }),
+  ],
 };
 
 module.exports = config;
