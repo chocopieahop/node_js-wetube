0bd75d2fa8728f80141cc43e769fd965d4df37f9
JeongEun <6rodnjf1@naver.com>
2020-09-08 22:34:52 +0900

    #9.2
    코드는 똑같은데 동영상저장까지 되고
    그런데 다운로드한 동영상은 검은화면!

--- static/main.js
+++ static/main.js
@@ -348,8 +348,33 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 var recorderContainer = document.getElementById("jsRecordContainer");
 var recordBtn = document.getElementById("jsRecordBtn");
 var videoPreview = document.getElementById("jsVideoPreview");
+var streamObject;
+var videoRecorder;
 
-var startRecording = /*#__PURE__*/function () {
+var handleVideoData = function handleVideoData(event) {
+  var videoFile = event.data;
+  var link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.mp4";
+  document.body.appendChild(link);
+  link.click();
+};
+
+var stopRecording = function stopRecording() {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+var startRecording = function startRecording() {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+var getVideo = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
     var stream;
     return regeneratorRuntime.wrap(function _callee$(_context) {
@@ -368,30 +393,37 @@ var startRecording = /*#__PURE__*/function () {
             videoPreview.srcObject = stream;
             videoPreview.muted = true;
             videoPreview.play();
-            _context.next = 13;
+            recordBtn.innerHTML = "Stop recording";
+            streamObject = stream;
+            startRecording();
+            _context.next = 15;
             break;
 
-          case 9:
-            _context.prev = 9;
+          case 12:
+            _context.prev = 12;
             _context.t0 = _context["catch"](0);
             recordBtn.innerHTML = " Can't record";
-            recordBtn.removeEventListener("click", startRecording);
 
-          case 13:
+          case 15:
+            _context.prev = 15;
+            recordBtn.removeEventListener("click", getVideo);
+            return _context.finish(15);
+
+          case 18:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, null, [[0, 9]]);
+    }, _callee, null, [[0, 12, 15, 18]]);
   }));
 
-  return function startRecording() {
+  return function getVideo() {
     return _ref.apply(this, arguments);
   };
 }();
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
